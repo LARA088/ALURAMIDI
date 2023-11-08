1612ce7fb44f7a21e806a3d80d11c04b56121c33
LARA088 <128611780+LARA088@users.noreply.github.com>
2023-11-08 11:12:17 -0300

    ok

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
+
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_vaia').play();
+}
+document.querySelector('.tecla_vaia').onclick=tocaSomAplausos;
+
+
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_risada').play();   
+}
+document.querySelector('.tecla_risada').onclick=tocaSomAplausos;
+
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_errado').play();   
+}
+document.querySelector('.tecla_errado').onclick=tocaSomAplausos;
+
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_grilo').play();   
+}
+document.querySelector('.tecla_grilo').onclick=tocaSomAplausos;
